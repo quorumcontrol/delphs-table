1ff685837a2edc5fbee4cbfb35a47662d680e36e
Topper Bowers <topper@toppingdesign.com>
2022-07-24 20:22:35 +0200

    fix battle ui

--- game/src/board/BattleUI.ts
+++ game/src/board/BattleUI.ts
@@ -8,7 +8,7 @@ import mustFindByName from "../utils/mustFindByName";
 import { randomBounded } from "../utils/randoms";
 import PlayerMarker from "./PlayerMarker";
 
-const standardPlaces:[number,number,number][] = [[0.2,8,0.35],[0.15,8,-0.2]]
+const standardPlaces:[number,number,number][] = [[0.2,43,0.35],[0.15,43.5,-0.2]]
 
 @createScript("battleUI")
 class BattleUI extends ScriptTypeBase {
@@ -92,7 +92,7 @@ class BattleUI extends ScriptTypeBase {
     this.entity.addChild(playerMarker);
     this.getScript<PlayerMarker>(playerMarker as Entity, 'playerMarker')?.setWarrior(warrior)
 
-    playerMarker.setLocalScale(0.1, 22.5, 0.1);
+    playerMarker.setLocalScale(0.1, 10, 0.1);
 
     playerMarker.setLocalPosition(...standardPlaces[index]);
     playerMarker.setRotation(0, randomBounded(0.2), 0, 1);
