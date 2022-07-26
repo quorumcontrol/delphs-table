3776b61a9a7decbb726fd048afb78d554fd639ed
Topper Bowers <topper@toppingdesign.com>
2022-07-26 10:41:24 +0200

    deploy the orchestrator

---
+++ delphs-table-site/contracts/deploy/004_OrchestratorState.ts
@@ -0,0 +1,18 @@
+import "hardhat-deploy";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+const func: DeployFunction = async function ({
+  deployments,
+  getNamedAccounts,
+}: HardhatRuntimeEnvironment) {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  await deploy("OrchestratorState", {
+    from: deployer,
+    log: true,
+    args: [deployer],
+  });
+};
+export default func;
