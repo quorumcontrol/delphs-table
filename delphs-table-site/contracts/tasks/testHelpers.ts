9a5cd1420679e94b11ac550aeb7e4797756be4d4
Topper Bowers <topper@toppingdesign.com>
2022-07-26 14:03:50 +0200

    generated new bots, fixed some errors and dpulicate code

--- delphs-table-site/contracts/tasks/testHelpers.ts
+++ delphs-table-site/contracts/tasks/testHelpers.ts
@@ -9,62 +9,6 @@ function hashString(msg:string) {
   return keccak256(Buffer.from(msg))
 }
 
-task('do-table', async (_,hre) => {
-  const lobby = await getLobbyContract(hre)
-  const delphs = await getDelphsTableContract(hre)
-  const deployer = await getDeployer(hre)
-  const player = await getPlayerContract(hre)
-
-  const waiting = await lobby.waitingAddresses()
-  const rounds = 100
-
-  const botNumber = Math.max(10 - waiting.length, 0)
-  const id = hashString(`${faker.company.companyName()}: ${faker.company.bs()}}`)
-
-  const playersWithNamesAndSeeds = (await Promise.all(waiting.concat((await getBots(botNumber)).map((b) => b.address)).map(async (address) => {
-    const name = await player.name(address)
-    if (!name) {
-      console.log(`${address} has no name`)
-      return null
-    }
-    return {
-      name,
-      address
-    }
-  })))
-    .filter((p) => !!p)
-    .map((p) => {
-      return {
-        ...p,
-        seed: hashString(`${id}-${player!.name}-${player!.address}`)
-      }
-    })
-
-  const tx = await delphs.createTable(id, playersWithNamesAndSeeds.map((p) => p.address!), playersWithNamesAndSeeds.map((p) => p.seed), rounds, deployer.address)
-  console.log('table id: ', id, 'tx: ', tx.hash)
-  await tx.wait()
-  await (await lobby.takeAddresses(waiting, id)).wait()
-  console.log('done')
-})
-
-task('xxx', async (_, hre) => {
-  const player = await getPlayerContract(hre)
-  // const filter = player.filters.DeviceAdded(null, '0xe109BDB684c84ee13e973cd837d9f27193A42cbE')
-  // const evts = await player.queryFilter(filter)
-  // console.log(evts)
-  // const lobby = await getLobbyContract(hre)
-  // const wallet = new Wallet('0x7ff2e7a183c3f83a46f83ad83c308810c2c16845c589f505002f5262a4a5696e').connect(hre.ethers.provider)
-  // console.log(await player.deviceToPlayer('0x126DAa9f37b065a4c67c3e51c4547bfC125A910e'))
-  // console.log(await player.deviceToPlayer(await wallet.getAddress()))
-  // const tx = await lobby.connect(wallet).registerInterest()
-  // console.log('tx hash: ', tx.hash)
-  // console.log(await tx.wait())
-
-
-  const tx = await hre.ethers.provider.getTransaction('0xba9144cd2942dd4ae33f8c6e5e6a76e5143354cc0369fa3d2d05731bf61702d5')
-  console.log('tx: ', tx)
-})
-
 task('start')
   .addParam('id')
   .setAction(async ({ id }, hre) => {
@@ -99,6 +43,7 @@ task('setup-bots', 'setup a number of bots')
         }
       })
       for (const wallet of wallets) {
+        console.log('creating ', wallet.name)
         const addr = await wallet.wallet.getAddress()
         await deployer.sendTransaction({
           to: addr,
@@ -120,14 +65,15 @@ task('setup-bots', 'setup a number of bots')
   })
 
 async function getBots(num:number) {
-  const { default: botSetup } = await import('../bots')
-  const botNames = Object.keys(botSetup)
-  return botNames.slice(0, num).map((name) => {
-    return {
-      name,
-      ...botSetup[name]
-    }
-  })
+    const { default: botSetup } = await import('../bots')
+    const botNames = Object.keys(botSetup)
+    return botNames.slice(0, num).map((name) => {
+      return {
+        name,
+        ...botSetup[name]
+      }
+    })
+
 }
 
 task('player')
@@ -185,43 +131,5 @@ task('board')
 
     const id = hashString(name)
     await (await delphs.createTable(id, tableAddrs, seeds, rounds, deployer.address)).wait()
-    console.log('table id: ', id)
-  })
-
-task('test-board')
-  .addParam('name')
-  .setAction(async ({ name }, hre) => {
-    const smallFuel = utils.parseEther('0.01')
-
-    const player1 = Wallet.createRandom().connect(hre.ethers.provider)
-    const player2 = Wallet.createRandom().connect(hre.ethers.provider)
-
-    const deployer = await getDeployer(hre)
-    const tx1 = await deployer.sendTransaction({
-      to: player1.address,
-      value: smallFuel
-    })
-    const tx2 = await deployer.sendTransaction({
-      to: player2.address,
-      value: smallFuel
-    })
-
-    await Promise.all([
-      tx1.wait(),
-      tx2.wait()
-    ])
-  
-    console.log(`
-    player1: ${await player1.getAddress()} ${player1.privateKey}
-    player2: ${await player2.getAddress()} ${player2.privateKey}
-    `)
-  
-    const player = await getPlayerContract(hre)
-    await player.connect(player1).initializePlayer(`${name} - 1`, player1.address)
-    await player.connect(player2).initializePlayer(`${name} - 2`, player2.address)
-
-    const delphs = await getDelphsTableContract(hre)
-    const id = hashString(name)
-    await (await delphs.createTable(id, [player1.address, player2.address], [hashString(`1-${name}`), hashString(`2-${name}`)], 60, deployer.address)).wait()
     console.log('table id: ', id)
   })
