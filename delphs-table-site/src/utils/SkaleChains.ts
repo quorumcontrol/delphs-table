c4afba6d59259d0f84eb78a3b3b25107550aeb6e
Topper Bowers <topper@toppingdesign.com>
2022-07-27 16:45:32 +0200

    update testnet in the site

--- delphs-table-site/src/utils/SkaleChains.ts
+++ delphs-table-site/src/utils/SkaleChains.ts
@@ -1,42 +1,41 @@
 import { Chain } from "@rainbow-me/rainbowkit";
 
 export const skaleTestnet: Chain = {
-  id: 132333505628089,
+  id: 1305754875840118,
   name: 'Skale Testnet',
   network: 'skaletestnet',
   iconUrl: '/SKALE_logo.svg',
-  iconBackground: '#fff',
   nativeCurrency: {
     decimals: 18,
     name: 'sFUEL',
     symbol: 'sFUEL',
   },
   rpcUrls: {
-    default: 'https://testnet-proxy.skalenodes.com/v1/whispering-turais',
-    wss: 'wss://testnet-proxy.skalenodes.com/v1/ws/whispering-turais',
+    default: 'https://staging-v2.skalenodes.com/v1/rapping-zuben-elakrab',
+    wss: 'wss://staging-v2.skalenodes.com/v1/ws/rapping-zuben-elakrab',
   },
   blockExplorers: {
-    default: { name: 'BlockScout', url: 'https://whispering-turais.testnet-explorer.skalenodes.com/' },
+    default: { name: 'BlockScout', url: 'https://rapping-zuben-elakrab.explorer.staging-v2.skalenodes.com/' },
   },
   testnet: true,
 };
 
-export const skaleMainnet: Chain = {
-  id: 1032942172,
-  name: 'Crypto Rome Network',
-  network: 'cryptorome',
-  iconUrl: '/SKALE_logo.svg',
-  iconBackground: '#fff',
-  nativeCurrency: {
-    decimals: 18,
-    name: 'sFUEL',
-    symbol: 'sFUEL',
-  },
-  rpcUrls: {
-    default: 'https://mainnet.skalenodes.com/v1/haunting-devoted-deneb',
-  },
-  blockExplorers: {
-    default: { name: 'BlockScout', url: 'https://haunting-devoted-deneb.explorer.mainnet.skalenodes.com/' },
-  },
-  testnet: false,
-}
+// export const skaleMainnet: Chain = {
+//   id: 1032942172,
+//   name: 'Crypto Rome Network',
+//   network: 'cryptorome',
+//   iconUrl: '/SKALE_logo.svg',
+//   iconBackground: '#fff',
+//   nativeCurrency: {
+//     decimals: 18,
+//     name: 'sFUEL',
+//     symbol: 'sFUEL',
+//   },
+//   rpcUrls: {
+//     default: 'https://mainnet.skalenodes.com/v1/haunting-devoted-deneb',
+//   },
+//   blockExplorers: {
+//     default: { name: 'BlockScout', url: 'https://haunting-devoted-deneb.explorer.mainnet.skalenodes.com/' },
+//   },
+//   testnet: false,
+// }
